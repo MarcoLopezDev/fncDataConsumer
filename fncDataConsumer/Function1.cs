c3f39a51359da884e994ca59a9c06b425c23c597
MarcoLopezDev <60006979+MarcoLopezDev@users.noreply.github.com>
2021-06-15 18:30:36 -0400

    Agregar archivos de proyecto.

---
+++ fncDataConsumer/Function1.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using fncDataConsumer.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace fncDataConsumer
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task RunAsync(
+
+            [ServiceBusTrigger(
+                    "data",
+                    Connection = "MyConn"
+            )] string myQueueItem,
+
+            [CosmosDB(
+                    databaseName:"dbUbicua",
+                                        collectionName:"Eventos",
+                    ConnectionStringSetting = "strCosmos"
+             )] IAsyncCollector<object> datos,
+
+            ILogger log)
+        {
+            try
+            {
+                log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+                var data = JsonConvert.DeserializeObject<Data>(myQueueItem);
+                await datos.AddAsync(data);
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"No es posible insertar datos: {ex.Message}");
+            }
+        }
+    }
+
+}
---
+++ fncDataConsumer/Models/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace fncDataConsumer.Models
+{
+    
+    class Data
+    {
+        [Key]
+        public string NameUser { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime EventDate { get; set; }
+        [Required]
+        public string message { get; set; }
+
+    }
+}
